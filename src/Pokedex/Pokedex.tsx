a2dd8257705de20644884112f6e6999185ac64fa
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2022-10-30 14:56:20 -0300

    Adding App bar on the main layout

--- src/Pokedex/Pokedex.tsx
+++ src/Pokedex/Pokedex.tsx
@@ -1,10 +1,12 @@
-import {Box, Container } from '@material-ui/core';
-import SearchAppBar from './components/SearchAppBar';
+import { Container } from '@mui/material';
+import { Box } from '@mui/system';
 
 import React, {useState, useEffect} from 'react'
 import { getPokemonDetails } from '../Pokemon/services/getPokemonDetails';
 import {listPokemons, PokemonListInterface} from '../Pokemon/services/listPokemons'
 import { PokemonDetail } from '../Pokemon/types/PokemonDetail';
+
+import ButtonAppBar from './components/ButtonAppBar';
 
 export const Pokedex: React.FC = () => {
 
@@ -25,23 +27,23 @@ export const Pokedex: React.FC = () => {
 
    return (
       <>
-         <SearchAppBar />
+      
+      <ButtonAppBar />
 
-         <Container maxWidth='lg'>
-            <Box sx={{m: 2}}>
-                  Pokemons: 
-                  {pokemons.map(pokemon => {
-                     return(
-                        <>
-                           <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon.name}</button>
-                        </>
-                     )
-                  })}  
-                     
-                  <h2>Pokemon selecionado: {selectedPokemon?.name || 'Nenhum pokemon selecionado!'}</h2>
-            </Box>
-         </Container>
-
+      <Container maxWidth='lg'>
+         <Box sx={{mt: 2}}>
+            Pokemons: 
+            {pokemons.map(pokemon => {
+               return(
+                  <>
+                     <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon.name}</button>
+                  </>
+               )
+            })}  
+                           
+            <h2>Pokemon selecionado: {selectedPokemon?.name || 'Nenhum pokemon selecionado!'}</h2>
+         </Box>
+      </Container>
       </>
    );
 };
